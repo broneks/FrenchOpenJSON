edc7c2341af76c4dce601823b50ab4bfa48d4ec3
Bszulc <bronekszulc@gmail.com>
2015-05-19 21:32:47 -0400

    refactored parser for the start of the 2015 tournament

---
+++ index.php
@@ -0,0 +1,152 @@
+<?php
+
+include 'parse.php';
+
+$day = 1;
+$writePath  = 'data/2015/';
+$filePrefix = 'q';
+
+//
+// Load Document
+//
+
+$doc = new DOMDocument();
+@$doc->loadHTMLFile('http://www.rolandgarros.com/en_FR/scores/completed_matches/day' . $day . '.html');
+
+$finder = new DOMXPath($doc);
+
+//
+// Target Nodes
+//
+
+$tables = $finder->query('//*[contains(@class, "scoringtable")]');
+$events = $finder->query('//*[contains(@class, "eventinfo")]');
+
+//
+// Variables
+//
+
+$eventType   = '';
+$matchType   = '';
+$womSinRound = '';
+$womDubRound = '';
+$menSinRound = '';
+$menDubRound = '';
+$mixedRound  = '';
+
+$count = 0;
+
+$output = array();
+
+//
+// Begin Parsing
+//
+
+foreach ($events as $event) {
+	if (!$eventType) {
+		$eventType = Parse::getEventType($event);
+	}
+
+	$count = Parse::countEvents($count, $event);
+
+	if (!$womSinRound && (strpos($event->textContent, 'Women\'s Singles') !== false)) {
+		$womSinRound = Parse::getRound($event);
+	} 
+	else if (!$womDubRound && (strpos($event->textContent, 'Women\'s Doubles') !== false)) {
+		$womDubRound = Parse::getRound($event);
+	}
+	else if (!$menSinRound && (strpos($event->textContent, 'Men\'s Singles') !== false)) {
+		$menSinRound = Parse::getRound($event);
+	}
+	else if (!$menDubRound && (strpos($event->textContent, 'Men\'s Doubles') !== false)) {
+		$menDubRound = Parse::getRound($event);
+	}
+	else if (!$mixedRound && (strpos($event->textContent, 'Mixed') !== false)) {;
+		$mixedRound = Parse::getRound($event);
+	}
+}
+
+//
+// Create JSON Structure
+//
+
+$output['day']    = $day;
+$output['events'] = $count;
+$output['type']   = $eventType;
+
+$output['data'] = array(
+	'womens' => array(
+		'singles' => array(
+			'round' => $womSinRound
+		),
+		'doubles' => array(
+			'round' => $womDubRound
+		)
+	),
+	'mens' => array(
+		'singles' => array(
+			'round' => $menSinRound
+		),
+		'doubles' => array(
+			'round' => $menDubRound
+		)
+	),
+	'mixed' => array(
+		'round' => $mixedRound
+  )
+);
+
+foreach ($tables as $table) {
+	$matchSummary = $table->firstChild->textContent;
+
+	if (Parse::isMatchType($matchSummary, 'womSin')) {
+		$gender    = 'womens';
+		$matchType = 'singles';
+		$match     = Parse::getSingle($table);
+	}  
+	else if (Parse::isMatchType($matchSummary, 'womDub')) {
+		$gender    = 'womens';
+		$matchType = 'doubles';
+		$match     = Parse::getDouble($table);
+	}  
+	else if (Parse::isMatchType($matchSummary, 'menSin')) {
+		$gender    = 'mens';
+		$matchType = 'singles';	
+		$match     = Parse::getSingle($table);
+	}  
+	else if (Parse::isMatchType($matchSummary, 'menDub')) {
+		$gender    = 'mens';
+		$matchType = 'doubles';
+		$match     = Parse::getDouble($table);
+	}  
+	else if (Parse::isMatchType($matchSummary, 'mixed')) {
+		$matchType = 'mixed';	
+		$match     = Parse::getDouble($table);
+	}
+
+	$matchArray = array(
+		'status' => $match['status'],
+		'court'  => $match['court'],
+		'winner' => $match['winner'],
+		'loser'  => $match['loser'],
+		'sets'   => $match['sets']
+	);
+
+	if ($gender) {
+		$output['data'][$gender][$matchType]['matches'][] = $matchArray;
+	} 
+	else {
+		$output['data']['mixed']['matches'][] = $matchArray;
+	}
+}
+
+//
+// Write to JSON File
+//
+
+header('content-type: application/json');
+echo json_encode($output, JSON_PRETTY_PRINT);
+
+$fp = fopen($writePath . $filePrefix . $day . '.json', 'w');
+fwrite($fp, json_encode($output, JSON_PRETTY_PRINT));
+fclose($fp);
--- parse.php
+++ parse.php
@@ -1,302 +1,202 @@
 <?php
 
-$day = 15;
-
-$doc = new DOMDocument();
-@$doc->loadHTMLFile('http://www.rolandgarros.com/en_FR/scores/completed_matches/day' . ($day + 5) . '.html');
-$finder = new DOMXPath($doc);
-
-$tables = $finder->query('//*[contains(@class, "scoringtable")]');
-$events = $finder->query('//*[contains(@class, "eventinfo")]');
-
-$type        = '';
-$womSinRound = '';
-$womDubRound = '';
-$menSinRound = '';
-$menDubRound = '';
-$mixedRound  = '';
-
-$count = 0;
-
-$obj = array();
-
-// parsing begins
-
-foreach ($events as $event) {
-	if (!$type && (strpos($event->textContent, 'Qualifications') !== false)) $type = 'Qualifications';
-
-	if (strpos($event->textContent, 'Women\'s Singles')	!== false ||
-		strpos($event->textContent, 'Women\'s Doubles')	!== false ||
-		strpos($event->textContent, 'Men\'s Singles')	!== false ||
-		strpos($event->textContent, 'Men\'s Doubles')	!== false ||
-		strpos($event->textContent, 'Mixed')			!== false) {
-		$count++;
-	}
-
-	if (!$womSinRound && (strpos($event->textContent, 'Women\'s Singles') !== false)) {
-		$dash = strpos($event->textContent, '-');
-		$womSinRound = substr($event->textContent, $dash + 2);
-	} 
-	else if (!$womDubRound && (strpos($event->textContent, 'Women\'s Doubles') !== false)) {
-		$dash = strpos($event->textContent, '-');
-		$womDubRound = substr($event->textContent, $dash + 2);
-	}
-	else if (!$menSinRound && (strpos($event->textContent, 'Men\'s Singles') !== false)) {
-		$dash = strpos($event->textContent, '-');
-		$menSinRound = substr($event->textContent, $dash + 2);
-	}
-	else if (!$menDubRound && (strpos($event->textContent, 'Men\'s Doubles') !== false)) {
-		$dash = strpos($event->textContent, '-');
-		$menDubRound = substr($event->textContent, $dash + 2);
-	}
-	else if (!$mixedRound && (strpos($event->textContent, 'Mixed') !== false)) {
-		$dash = strpos($event->textContent, '-');
-		$mixedRound = substr($event->textContent, $dash + 2);
-	}
-}
-
-$obj['day']    = $day;
-$obj['events'] = $count;
-$obj['type']   = ($type) ? $type : 'Tournament';
-
-$obj['data'] = array(
-	'womens' => array(
-		'singles' => array(
-			'round' => $womSinRound
-		),
-		'doubles' => array(
-			'round' => $womDubRound
-		)
-	),
-	'mens'   => array(
-		'singles' => array(
-			'round' => $menSinRound
-		),
-		'doubles' => array(
-			'round' => $menDubRound
-		)
-	),
-	'mixed'  => array(
-		'round' => $mixedRound
-	)
-);
-
-foreach ($tables as $node) {
-	$type = '';
-	
-	if (strpos($node->firstChild->textContent, 'Women\'s Singles') !== false) {
-		$gender = 'womens';
-		$type   = 'singles';
-		$s = getSingle($node);
-	}  
-	else if (strpos($node->firstChild->textContent, 'Women\'s Doubles') !== false) {
-		$gender = 'womens';
-		$type   = 'doubles';
-		$d = getDouble($node);
-	}  
-	else if (strpos($node->firstChild->textContent, 'Men\'s Singles') !== false) {
-		$gender = 'mens';
-		$type   = 'singles';	
-		$s = getSingle($node);
-	}  
-	else if (strpos($node->firstChild->textContent, 'Men\'s Doubles') !== false) {
-		$gender = 'mens';
-		$type   = 'doubles';
-		$d = getDouble($node);
-	}  
-	else if (strpos($node->firstChild->textContent, 'Mixed') !== false) {
-		$type   = 'mixed';	
-		$m = getDouble($node);
-	}  
-
-	switch ($type) {
-		case 'singles':
-			$obj['data'][$gender][$type]['matches'][] = array(
-				'status' => $s['status'],
-				'court'  => $s['court'],
-				'winner' => $s['winner'],
-				'loser'  => $s['loser'],
-				'sets'   => $s['sets']
-			);
-			break;
-
-		case 'doubles':
-			$obj['data'][$gender][$type]['matches'][] = array(
-				'status'  => $d['status'],
-				'court'   => $d['court'],
-				'winners' => $d['winners'],
-				'losers'  => $d['losers'],
-				'sets'    => $d['sets']
-			);
-			break;
-
-		case 'mixed':
-			$obj['data']['mixed']['matches'][] = array(
-				'status'  => $m['status'],
-				'court'   => $m['court'],
-				'winners' => $m['winners'],
-				'losers'  => $m['losers'],
-				'sets'    => $m['sets']
-			);
-			break;
-	}
-}
-
-function getSingle($node) {
-	$trimmedStatus = trim($node->childNodes->item(3)->childNodes->item(1)->textContent, chr(0xC2).chr(0xA0));
-	$status  = ($trimmedStatus) ? $trimmedStatus : 'Completed';
-	$court   = $node->childNodes->item(1)->textContent;
-	$sets    = array();
-	$player1 = $node->childNodes->item(3)->firstChild->childNodes;
-	$player2 = $node->childNodes->item(3)->childNodes->item(2)->childNodes;
-
-	// players
-	// if player1 is the winner
-	if (substr($player1->item(1)->getAttribute('class'), 8) === 'winner') {
-		$p1win = true;
-
-		$winner = array(
-			'name'    => $player1->item(0)->getElementsByTagName('a')->item(0)->textContent,
-			'country' => substr($player1->item(0)->getElementsByTagName('span')->item(0)->textContent, 1, 3)
-		);
-
-		$loser = array(
-			'name'    => $player2->item(0)->getElementsByTagName('a')->item(0)->textContent,
-			'country' => substr($player2->item(0)->getElementsByTagName('span')->item(0)->textContent, 1, 3)
-		);
-	} 
-	else {
-		$winner = array(
-			'name'    => $player2->item(0)->getElementsByTagName('a')->item(0)->textContent,
-			'country' => substr($player2->item(0)->getElementsByTagName('span')->item(0)->textContent, 1, 3)
-		);
-
-		$loser = array(
-			'name'    => $player1->item(0)->getElementsByTagName('a')->item(0)->textContent,
-			'country' => substr($player1->item(0)->getElementsByTagName('span')->item(0)->textContent, 1, 3)
-		);
-	}
-
-	// sets
-	for ($i = 3; $i < 8; $i++) {
-		if ($player1->item($i)->childNodes->item(0)->textContent === '') break;
-		
-		$p1score = $player1->item($i)->childNodes->item(0)->textContent;
-		$p2score = $player2->item($i)->childNodes->item(0)->textContent;
-		
-		if (isset($p1win)) {
-			$sets[] = array(
-				(strlen($p1score) > 1 && substr($p1score, 0, 1) !== '[') ? substr($p1score, 0, 1) . '(' . substr($p1score, 1) . ')' : $p1score,
-				(strlen($p2score) > 1 && substr($p1score, 0, 1) !== '[') ? substr($p2score, 0, 1) . '(' . substr($p2score, 1) . ')' : $p2score
-			);
-		} else {
-			$sets[] = array(
-				(strlen($p2score) > 1 && substr($p1score, 0, 1) !== '[') ? substr($p2score, 0, 1) . '(' . substr($p2score, 1) . ')' : $p2score,
-				(strlen($p1score) > 1 && substr($p1score, 0, 1) !== '[') ? substr($p1score, 0, 1) . '(' . substr($p1score, 1) . ')' : $p1score
-			);
-		}
-	}
-
-	return array(
-		'status' => $status,
-		'court'  => $court,
-		'winner' => $winner, 
-		'loser'  => $loser,
-		'sets'   => $sets
+class Parse {
+	private static $types = array(
+		"womSin" => "Women's Singles",
+		"womDub" => "Women's Doubles",
+		"menSin" => "Men's Singles",
+		"menDub" => "Men's Doubles",
+		"mixed"  => "Mixed"
 	);
-}
 
-function getDouble($node) {
-	$trimmedStatus = trim($node->childNodes->item(3)->childNodes->item(1)->textContent, chr(0xC2).chr(0xA0));
-	$status  = ($trimmedStatus) ? $trimmedStatus : 'Completed';
-	$court  = $node->childNodes->item(1)->textContent;
-	$sets   = array();
-	$team1 = $node->childNodes->item(3)->firstChild->childNodes;
-	$team2 = $node->childNodes->item(3)->childNodes->item(2)->childNodes;
+	//
+	// Event Info
+	//
 
-	// teams
-	// if team 1 is the winner
-	if (substr($team1->item(1)->getAttribute('class'), 8) === 'winner') {
-		$t1win = true;
+	public static function getEventType($event) {
+		if (strpos($event->textContent, 'Qualifications') !== false) {
+			return 'Qualifications';
+		}
 
-		$winners = array(
-			'names' => array(
-				$team1->item(0)->getElementsByTagName('a')->item(0)->textContent,
-				$team1->item(0)->getElementsByTagName('a')->item(1)->textContent
-			),
-			'countries' => array(
-				substr($team1->item(0)->getElementsByTagName('span')->item(0)->textContent, 1, 3),
-				substr($team1->item(0)->getElementsByTagName('span')->item(1)->textContent, 1, 3)
-			)
-		);
+		return 'Tournament';
+	}
 
-		$losers = array(
-			'names' => array(
-				$team2->item(0)->getElementsByTagName('a')->item(0)->textContent,
-				$team2->item(0)->getElementsByTagName('a')->item(1)->textContent
-			),
-			'countries' => array(
-				substr($team2->item(0)->getElementsByTagName('span')->item(0)->textContent, 1, 3),
-				substr($team2->item(0)->getElementsByTagName('span')->item(1)->textContent, 1, 3)
-			)
-		);
-	} else {
-		$winners = array(
-			'names' => array(
-				$team2->item(0)->getElementsByTagName('a')->item(0)->textContent,
-				$team2->item(0)->getElementsByTagName('a')->item(1)->textContent
-			),
-			'countries' => array(
-				substr($team2->item(0)->getElementsByTagName('span')->item(0)->textContent, 1, 3),
-				substr($team2->item(0)->getElementsByTagName('span')->item(1)->textContent, 1, 3)
-			)
-		);
+	public static function countEvents($count, $event) {
+		$eventText = $event->textContent;
+		$types     = array_values(self::$types);
 
-		$losers = array(
-			'names' => array(
-				$team1->item(0)->getElementsByTagName('a')->item(0)->textContent,
-				$team1->item(0)->getElementsByTagName('a')->item(1)->textContent
-			),
-			'countries' => array(
-				substr($team1->item(0)->getElementsByTagName('span')->item(0)->textContent, 1, 3),
-				substr($team1->item(0)->getElementsByTagName('span')->item(1)->textContent, 1, 3)
-			)
+		foreach ($types as $type) {
+			if (strpos($eventText, $type) !== false) {
+				$count++;
+			}
+		}
+
+		return $count;
+	}
+
+	public static function getRound($event) {
+		$dash  = strpos($event->textContent, '-');
+		$round = substr($event->textContent, $dash + 2);
+
+		return $round;
+	}
+
+	//
+	// Match Info
+	//
+
+	private static function getName($side) {
+		return $side->item(0)->getElementsByTagName('a')->item(0)->textContent;
+	}
+
+	private static function getCountry($side) {
+		return substr($side->item(0)->getElementsByTagName('span')->item(0)->textContent, 1, 3);
+	}
+
+	private static function getBasicMatchInfo($node) {
+		$matchNode = $node->childNodes->item(1)->childNodes->item(0)->childNodes;
+		
+		$court  = $node->childNodes->item(0)->childNodes->item(0)->textContent;
+		$status = $matchNode->item(1)->childNodes->item(0)->textContent;
+		$status = $status === 'Complete' ? 'Completed' : $status;
+
+		$side1  = $matchNode->item(0)->childNodes;
+		$side2  = $matchNode->item(2)->childNodes;
+
+		return array(
+			'court'  => $court,
+			'status' => $status,
+			'side1'  => $side1,
+			'side2'  => $side2,
+			'sets'   => array()
 		);
 	}
 
-	// sets
-	for ($i = 3; $i < 8; $i++) {
-		if ($team1->item($i)->childNodes->item(0)->textContent === '') break;
+	private static function formatScore($score) {
+		if (strlen($score) > 1) {
+			return substr($score, 0, 1) . '(' . substr($score, 1) . ')';
+		}
 
-		$t1score = $team1->item($i)->childNodes->item(0)->textContent;
-		$t2score = $team2->item($i)->childNodes->item(0)->textContent;
-		
-		if (isset($t1win)) {
-			$sets[] = array(
-				(strlen($t1score) > 1 && substr($t1score, 0, 1) !== '[') ? substr($t1score, 0, 1) . '(' . substr($t1score, 1) . ')' : $t1score,
-				(strlen($t2score) > 1 && substr($t1score, 0, 1) !== '[') ? substr($t2score, 0, 1) . '(' . substr($t2score, 1) . ')' : $t2score
+		return $score;
+	}
+
+	private static function getSets($side1Win, $side1, $side2) {
+		$sets = array();
+
+		for ($i = 3; $i < 8; $i++) {
+			$side1Score = $side1->item($i)->childNodes->item(0)->textContent;
+			$side2Score = $side2->item($i)->childNodes->item(0)->textContent;
+			
+			// no more sets
+			if ($side1Score === '' && $side2Score === '') break;
+
+			$side1Score = self::formatScore($side1Score);
+			$side2Score = self::formatScore($side2Score);
+
+			if ($side1Win) {
+				$sets[] = array($side1Score, $side2Score);
+			} 
+			else {
+				$sets[] = array($side2Score, $side1Score);
+			}
+		}
+
+		return $sets;
+	}
+
+	public static function isMatchType($text, $key) {
+		return strpos($text, self::$types[$key]) !== false;
+	}
+
+	public static function getSingle($node) {
+		$match = self::getBasicMatchInfo($node);
+		$sides = array();
+
+		if (strpos($match['side1']->item(1)->getAttribute('class'), 'winner') !== false) {
+			$side1Win = true;
+
+			$sides['winner'] = array(
+				'name'    => self::getName($match['side1']),
+				'country' => self::getCountry($match['side1'])
+			);
+			$sides['loser'] = array(
+				'name'    => self::getName($match['side2']),
+				'country' => self::getCountry($match['side2'])
 			);
 		} 
 		else {
-			$sets[] = array(
-				(strlen($t2score) > 1 && substr($t1score, 0, 1) !== '[') ? substr($t2score, 0, 1) . '(' . substr($t2score, 1) . ')' : $t2score,
-				(strlen($t1score) > 1 && substr($t1score, 0, 1) !== '[') ? substr($t1score, 0, 1) . '(' . substr($t1score, 1) . ')' : $t1score
+			$side1Win = false;
+
+			$sides['winner'] = array(
+				'name'    => self::getName($match['side2']),
+				'country' => self::getCountry($match['side2'])
+			);
+			$sides['loser']  = array(
+				'name'    => self::getName($match['side1']),
+				'country' => self::getCountry($match['side1'])
 			);
 		}
+
+		$sides['sets'] = self::getSets($side1Win, $match['side1'], $match['side2']);
+
+		return array_merge($match, $sides);
 	}
 
-	return array(
-		'status'  => $status,
- 		'court'   => $court,
-		'winners' => $winners, 
-		'losers'  => $losers,
-		'sets'    => $sets
-	);
+	public static function getDouble($node) {
+		//
+		// TO BE COMPLETED
+		//
+
+
+		// $match = self::getBasicMatchInfo($node);
+		// $sides = array();
+
+		// if (strpos($match['side1']->item(1)->getAttribute('class'), 'winner') !== false) {
+		// 	$t1win = true;
+
+		// 	$winners = array(
+		// 		'names' => array(
+		// 			$team1->item(0)->getElementsByTagName('a')->item(0)->textContent,
+		// 			$team1->item(0)->getElementsByTagName('a')->item(1)->textContent
+		// 		),
+		// 		'countries' => array(
+		// 			substr($team1->item(0)->getElementsByTagName('span')->item(0)->textContent, 1, 3),
+		// 			substr($team1->item(0)->getElementsByTagName('span')->item(1)->textContent, 1, 3)
+		// 		)
+		// 	);
+
+		// 	$losers = array(
+		// 		'names' => array(
+		// 			$team2->item(0)->getElementsByTagName('a')->item(0)->textContent,
+		// 			$team2->item(0)->getElementsByTagName('a')->item(1)->textContent
+		// 		),
+		// 		'countries' => array(
+		// 			substr($team2->item(0)->getElementsByTagName('span')->item(0)->textContent, 1, 3),
+		// 			substr($team2->item(0)->getElementsByTagName('span')->item(1)->textContent, 1, 3)
+		// 		)
+		// 	);
+		// } else {
+		// 	$winners = array(
+		// 		'names' => array(
+		// 			$team2->item(0)->getElementsByTagName('a')->item(0)->textContent,
+		// 			$team2->item(0)->getElementsByTagName('a')->item(1)->textContent
+		// 		),
+		// 		'countries' => array(
+		// 			substr($team2->item(0)->getElementsByTagName('span')->item(0)->textContent, 1, 3),
+		// 			substr($team2->item(0)->getElementsByTagName('span')->item(1)->textContent, 1, 3)
+		// 		)
+		// 	);
+
+		// 	$losers = array(
+		// 		'names' => array(
+		// 			$team1->item(0)->getElementsByTagName('a')->item(0)->textContent,
+		// 			$team1->item(0)->getElementsByTagName('a')->item(1)->textContent
+		// 		),
+		// 		'countries' => array(
+		// 			substr($team1->item(0)->getElementsByTagName('span')->item(0)->textContent, 1, 3),
+		// 			substr($team1->item(0)->getElementsByTagName('span')->item(1)->textContent, 1, 3)
+		// 		)
+		// 	);
+		// }
+	}
 }
-
-header('content-Type: application/json');
-echo json_encode($obj, JSON_PRETTY_PRINT);
-
-$fp = fopen('data/day' . $day . '.json', 'w');
-fwrite($fp, json_encode($obj, JSON_PRETTY_PRINT));
-fclose($fp);
